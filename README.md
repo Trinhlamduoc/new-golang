041eb73c30db392a3802eae55d42138150dbe230
Đăng Trình <dtrinhit04@gmail.com>
2026-07-15 11:02:23 +0700

    method

---
+++ README.md
@@ -0,0 +1,11 @@
+GOLANG
+
+<!-- method -->
+Counter là recever phần nhận vào
+String() là tên hàm, chỉ dùn 1 recever cụ thể
+
+method là function nhưng dữ liệu nhận vào của method là kiểu struct hoặc non-struct
+
+lưu ý:
+Nếu muốn dùng tên của func giống nhau thì phải có recever khác nhau
+Các method có thể trùng tên nếu xác định trên các dữ liệu khác nhau trong khi các hàm thì không được phép trùng tên
--- method.go
+++ method.go
@@ -6,12 +6,20 @@ import "fmt"
 
 type Counter int 
 
-func (c Counter) String() string {
-	return fmt.Sprintf("Counter(%d)", c)
+// Counter là recever phần nhận vào
+// String() là tên hàm, chỉ dùn 1 recever cụ thể
+// method là function nhưng dữ liệu nhận vào của method là kiểu struct hoặc non-struct
+
+// lưu ý:
+// Nếu muốn dùng tên của func giống nhau thì phải có recever khác nhau
+// Các method có thể trùng tên nếu xác định trên các dữ liệu khác nhau trong khi các hàm thì không được phép trùng tên
+func (c Counter) String2() string {
+	return fmt.Sprintf("Counter(%d) 111", c)
 }
 
 func main() {
-	var c Counter = 3
-	fmt.Println(c.String())
+	var c Counter = 2
+	fmt.Println(c.String2())
 	
+
 }
