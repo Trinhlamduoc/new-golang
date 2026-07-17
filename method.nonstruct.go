1c7df220ed70c6f19255c37fcf914a71da9b75b5
Đăng Trình <dtrinhit04@gmail.com>
2026-07-17 19:28:09 +0700

    add note method nonstruct

--- method.nonstruct.go
+++ method.nonstruct.go
@@ -10,6 +10,7 @@ type MyFloat float64
 // trong method Abs không thể khai báo receiver trực tiếp là float64 được, phải thông qua tạo kiểu mới là type
 // vì trong Go không cho phép khai báo method trên kiểu được định nghĩa ở package khác, float64 là kiểu dựng sẵn của Go
 
+// Ghi nhớ: Chỉ được thêm method cho kiểu packge của mình định nghĩa
 func (f MyFloat) Abs() float64 {
 	if f < 0 {
 		return float64(-f)
