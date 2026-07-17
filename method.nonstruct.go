886579313625cfeeac5cc930ad2f906124ae1d58
Đăng Trình <dtrinhit04@gmail.com>
2026-07-17 19:26:12 +0700

    method non struct

--- method.nonstruct.go
+++ method.nonstruct.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+
+type MyFloat float64
+// trong method Abs không thể khai báo receiver trực tiếp là float64 được, phải thông qua tạo kiểu mới là type
+// vì trong Go không cho phép khai báo method trên kiểu được định nghĩa ở package khác, float64 là kiểu dựng sẵn của Go
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
 
 func main() {
-	
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println(f.Abs())
 }
