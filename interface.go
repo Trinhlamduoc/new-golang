e3e7a8d7f99a85a6751406691f31821b316aa8df
Đăng Trình <dtrinhit04@gmail.com>
2026-07-20 13:21:56 +0700

    switch type

--- interface.go
+++ interface.go
@@ -3,7 +3,8 @@ package main
 import (
 	"fmt"
 )
-// interface là 1 kiểu được định nghĩa bởi tập hợp các method, 
+
+// interface là 1 kiểu được định nghĩa bởi tập hợp các method,
 // interface có thể chứa bất kỳ giá trị gì miễn nó có implement các method này
 
 // khai báo interface
@@ -14,9 +15,21 @@ type Speaker interface {
 // cú pháp interface rỗng
 // interface{}
 
+// switch case type
+func do(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Println("Got an integer:", v)
+	case string:
+		fmt.Println("Got a string:", v)
+	default:
+		fmt.Println("Got something else:", v)
+	}
+}
+
 type Foo struct{}
 
-func(Foo) Speak() string {
+func (Foo) Speak() string {
 	return "Hello, I am Foo"
 }
 
